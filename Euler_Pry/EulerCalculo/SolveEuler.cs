e0a9b810989cbf0517b2c2709d89121c6bf98857
Diego López <diego:loport@gmail.com>
2017-01-13 17:20:03 -0500

    Solucion Problema 18

---
+++ Euler_Pry/EulerCalculo/SolveEuler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace EulerCalculo
+{
+    
+    public class Problema18
+    {
+        public long Calcular()
+        {
+            var archivoDatos = @"C:\Euler\E18.txt";
+            //var archivoDatos = @"C:\Euler\E182.txt";
+
+            return SolucionMaximaSuma.CalculaArchivo(archivoDatos);
+        }
+    }
+
+
+    public static class SolucionMaximaSuma
+    {
+        public static int CalculaArchivo(string archivoDatos)
+        {
+            string datos = File.ReadAllText(archivoDatos);
+
+            return Calcular(datos);
+        }
+
+        public static int Calcular(string datos)
+        {
+            var rows = datos
+                .Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(line =>
+                        line.Split(' ')
+                            .Select(token => int.Parse(token))
+                            .ToList()
+                )
+                .ToList();
+
+            return rows.Aggregate(
+                new List<int> { 0 },
+                (currentMax, nextRow) =>
+                {
+                    var rowLength = nextRow.Count();
+                    return nextRow
+                        .Select((cell, index) =>
+                                index == 0 ? cell + currentMax[index]
+                                : index == rowLength - 1 ? cell + currentMax[index - 1]
+                                : Math.Max(cell + currentMax[index - 1], cell + currentMax[index]))
+                        .ToList();
+                }
+                )
+                .Max();
+        }
+    }
+}
---
+++ Euler_Pry/EulerTest/uiEulerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EulerCalculo;
+
+namespace EulerTest
+{
+    [TestClass]
+    public class uiEulerTest
+    {
+
+
+        [TestMethod]
+        public void EulerTest()
+        {
+
+            long expected = 23;
+            long result = new Problema18().Calcular();
+
+            Assert.AreEqual(expected, result, 1, "No se calculo correctamente");
+
+
+
+        }
+    }
+}
